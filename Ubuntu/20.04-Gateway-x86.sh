e2208d8a6c19b9869c38a6cf20d76ad8f273ce15
Askr <m3933030@gmail.com>
2020-12-15 00:47:09 +0800

    add

--- Ubuntu/20.04-Gateway-x86.sh
+++ Ubuntu/20.04-Gateway-x86.sh
@@ -11,31 +11,26 @@ dns_server_backup="8.8.8.8"
 
 
 # 配置外网(pppoe) # TODO 根据配置的端口，配置WAN和Lan
-apt install -y pppoeconf trojan privoxy
+apt install -y pppoeconf trojan privoxy bind9 bind9-utils isc-dhcp-server
 
 # ## read pppoe config
 # read -p "Enter your pppoe-account:" pppoe-account
 # read -p "Enter your pppoe-password:" pppoe-password
 
 # ## set pppoe config(pass)
-
 pppoeconf
 
-# TODO 设置局域网大包
-# TDDO 设置 /etc/dhcp/dhcpd.conf dns 服务器 
-# 配置dhcp服务器 /etc/default/isc-dhcp-server /etc/dhcp/dhcpd.conf
-apt install -y isc-dhcp-server
+# set dhcp server
 sed -i "s/INTERFACESv4=\"\"/INTERFACESv4=\"${lan_interfate}\"/" /etc/default/isc-dhcp-server
-
 echo "\
 subnet 192.168.1.0 netmask 255.255.255.0 {
   range 192.168.1.2 192.168.1.230;
   option routers 192.168.1.1;
+  default-lease-time 600;
+  max-lease-time 7200;
 }
 " >> /etc/dhcp/dhcpd.conf
 
-
-## 配置dns服务器(powerDns)
 
 ## 配置iptables 上网
 iptables -t nat -A POSTROUTING -s 192.168.1.0/24 -o $wan_interface -j MASQUERADE
@@ -72,3 +67,11 @@ network:
 " > /etc/netplan/01-network-manager-all.yaml
 
 init 6
+
+# TODO 配置DNS(BIND)
+# TODO 修改dhcp-dns server
+# TODO 删除 pppoe 传输的dns 服务器，手动配置pppoe
+# TODO 配置privoxy
+# TODO 配置frp
+# TODO 配置nginx
+# TODO 尝试配置局域网大包
